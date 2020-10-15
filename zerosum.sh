38018bd96123a79d21102b5e9eaa7a29151cc7fe
manikiranp <manikiranp3@gmail.com>
2020-10-15 13:34:00 +0530

    zero sum modification

--- zerosum.sh
+++ zerosum.sh
@@ -1,19 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash 
 #4
-read -p "Enter total number to be input:" num
+read -p "Enter total number to be input:" n
 echo "Enter the integers: "
 read -a arr
-echo ${arr[@]
-IFS=$'' sorted=($(sort <<<"${arr[@]}")); unset IFS
-printf "%s\n" "${sorted[@]}"
+echo ${arr[@]}
+IFS=$'' sorted=($(sort <<<"${arr[*]}"))
+echo "${sorted[@]}"
 
 for (( i=0; i<=$(($n-2)); i++ ))
 do
-	for (( j=(($i+1)); j<=(($n-1)); j++ ))
+	for (( j=$(($i+1)); j<=$(($n-1)); j++ ))
 	do
-		for (( k=(($j+1)); k<=$n; k++ ))
+		for (( k=$(($j+1)); k<=$n; k++ ))
 		do
-		if [ $((${arr[i]}+${arr[j]}+${arr[k]} -eq 0 ]; then
+		x=$((${arr[i]}+${arr[j]}))
+		if [ $(($x+${arr[k]})) -eq 0 ]; then
 		echo "${arr[i]} ${arr[j]} ${arr[k]}"
 		fi
 		done
